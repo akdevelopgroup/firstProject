4190fb1f9a4ab17f0123559c257791eae45e4661
Kirill Moskalevich <mosk9364@gmail.com>
2020-01-03 01:24:10 +0300

    feat:  deployed react native

---
+++ devComands.md
@@ -0,0 +1,16 @@
+1. npm install -g expo-cli
+2. expo init
+
+Before running your app on iOS, make sure you have CocoaPods installed and initialize the project:
+
+  cd dotaHelper/ios
+  pod install
+
+Then you can run the project:
+
+  cd dotaHelper
+  yarn android
+  yarn ios
+
+
+https://stackoverflow.com/questions/58120990/how-to-resolve-the-error-on-react-native-start
---
+++ reminder/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+const { width } = Dimensions.get('screen');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image 
+        style={{width, height: 300}}
+        source={require('./assets/logo.png')}
+      />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Педоры идуть</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
